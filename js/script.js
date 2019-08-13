253875e3e54a61d172afddbc620117c4caeaa3bb
Mariusz Jabłoński <amidek321@gmail.com>
2019-08-13 18:48:29 +0200

    instalacja i dodanie eslint, usunięcie błędów z pliku script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const titleClickHandler = function(event){
   event.preventDefault();
   const clickedElement = this;
@@ -7,43 +6,31 @@ const titleClickHandler = function(event){
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
-
   clickedElement.classList.add('active');
-
   const activeArticles = document.querySelectorAll('.posts article.active');
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
-
-  const articleSelector = clickedElement.getAttribute("href");
-
+  const articleSelector = clickedElement.getAttribute('href');
   const targetArticle = document.querySelector(articleSelector);
-    targetArticle.classList.add('active');
-  }
-
+  targetArticle.classList.add('active');
+};
 const optArticleSelector = '.post';
 const optTitleSelector = '.post-title';
 const optTitleListSelector = '.titles';
-
 function generateTitleLinks(){
   const titleList = document.querySelector(optTitleListSelector);
   titleList.innerHTML = '';
-
   const articles = document.querySelectorAll(optArticleSelector);
   let html = '';
   for (let article of articles) {
     article.classList.remove('active');
-
     const articleId = article.getAttribute('id');
-
     const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-
     const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
-
     html = html + linkHTML;
   }
   titleList.innerHTML = html;
-
   const links = document.querySelectorAll('.titles a');
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
